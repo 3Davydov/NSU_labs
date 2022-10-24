ccfdc6216e140d6800206ce9c6fabb65ec00242d
3Davydov <davydovdaniil501@gmail.com>
2022-10-24 20:42:54 +0700

    remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/davyd/Desktop/lab_BigInt
-# Build directory: C:/Users/davyd/Desktop/lab_BigInt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("Tests")
